f3277c278d1beb75ebd365ddeeeeb6703e1e6ae8
Felix Chan <hongkongfelix@gmail.com>
2019-10-18 13:16:09 +0800

    Update group to table

--- image_tools/create_json.py
+++ image_tools/create_json.py
@@ -1,28 +1,50 @@
 import json
+import sys
 
-total_groups = 9
+total_groups = 16
+if len(sys.argv) > 1:
+    total_groups = int(sys.argv[1])
+
 base_dir = '/files/upload_images/'
 
-img_list = []
+if total_groups > 1:
+    print('There are {} groups in total'.format(
+        total_groups
+    ))
+    
+    img_list = []
 
-for i in range(total_groups):
-    group_id = i+1
-    img_list.append({
+    for i in range(total_groups):
+        group_id = i+1
+        img_list.append({
 
-        'name': 'Group {}'.format(group_id),
-        'full': '{dir}large/group{id:02d}.jpg'.format(
-            dir=base_dir,
-            id=group_id
+            'name': 'Table {}'.format(group_id),
+            'full': '{dir}large/group{id:02d}.jpg'.format(
+                dir=base_dir,
+                id=group_id
+            ),
+            'small': '{dir}small/group{id:02d}.jpg'.format(
+                dir=base_dir,
+                id=group_id
+            ),
+            'large': '{dir}large/group{id:02d}.jpg'.format(
+                dir=base_dir,
+                id=group_id
+            ),
+        })
+else:
+    img_list = [{
+        'name': 'QR code',
+        'full': '{dir}/qr_code.png'.format(
+            dir=base_dir
         ),
-        'small': '{dir}small/group{id:02d}.jpg'.format(
-            dir=base_dir,
-            id=group_id
+        'small': '{dir}/qr_code.png'.format(
+            dir=base_dir
         ),
-        'large': '{dir}large/group{id:02d}.jpg'.format(
-            dir=base_dir,
-            id=group_id
-        ),
-    })
+        'large': '{dir}/qr_code.png'.format(
+            dir=base_dir
+        )
+    }]
 
 json_object = {
     'total_gp': total_groups,
